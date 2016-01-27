7268333991cfd09422c2c6383649e0b9e017cde7
Masaya Takeda <844196@gmail.com>
2016-01-27 19:37:11 +0900

    オプション解析を実装 #3

--- log_zipper.rb
+++ log_zipper.rb
@@ -3,19 +3,35 @@
 lib = File.expand_path('../lib', __FILE__)
 $LOAD_PATH.unshift(lib) unless $LOAD_PATH.include?(lib)
 require 'log_zipper'
+require 'optparse'
 
-config = LogZipper.config
-arg = ARGV[0].encode('UTF-8')
+params = {}
+args = ARGV.map {|arg| arg.encode('UTF-8') }
 
-log = LogZipper::Log.new(arg)
-
-log.convert do |additional|
-  additional.sort_rows    config.export_sort_order
-  additional.sort_columns config.export_fields
+OptionParser.new do |opt|
+  opt.banner = "Usage: #{File.basename(__FILE__)} [OPTIONS] <FILE1> <FILE2>..."
+  opt.version = LogZipper::VERSION
+  opt.on('-y', '--yaml=<PATH>', 'specifie user config.yml path') {|v| params[:yaml_path] = v }
+  opt.on('-l', '--log-level=<LEVEL>', 'specifie log level (default: WARN)') {|v| params[:log_level] = v.upcase }
+  opt.on('-d', '--dry-run', TrueClass) {|v| params[:dry_run] = v }
+  opt.parse!(args)
 end
-log.name << config.export_file_suffix
 
-File.open(log.path, 'wb') do |csv|
-  csv.print UTF8_BOM = "\xEF\xBB\xBF"
-  csv.print log.to_csv
+args.each do |path|
+  LogZipper.logger.level = eval("Logger::#{params[:log_level]}") if params[:log_level]
+  LogZipper.yaml_load :path => params[:yaml_path] if params[:yaml_path]
+
+  log = LogZipper::Log.new(path)
+  log.convert do |additional|
+    additional.sort_rows    LogZipper.config.export_sort_order
+    additional.sort_columns LogZipper.config.export_fields
+  end
+  log.name << LogZipper.config.export_file_suffix
+
+  (puts log.to_csv; next) if params[:dry_run]
+
+  File.open(log.path, 'wb') do |csv|
+    csv.print UTF8_BOM = "\xEF\xBB\xBF"
+    csv.print log.to_csv
+  end
 end
